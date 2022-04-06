24fef22151ed8435654175102daff858599adb94
monniiaa <sofielarsen2411@gmail.com>
2022-04-06 12:00:16 +0200

    made adjust brightness script

---
+++ Assets/Scripts/AdjustBrightness.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdjustBrightness : MonoBehaviour
+{
+    [SerializeField]
+    Slider adjustSlider;
+
+    SensorManager SensorManager;
+    ToggleSensors ToggleSensors;
+
+
+
+
+}
--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -8,17 +8,17 @@ public abstract class ButtonScript : MonoBehaviour
     //Same principle as creating a variable and not assigning it a value, because it can have different values at different times.
     //The values of the variables and the body of the methods can be customized for the individual objects that need the variables/methods. 
 
-    protected abstract Button button;
+    protected  Button button;
 
-    protected abstract bool isTriggered;
+    protected bool isTriggered;
 
     //protected SoundManager soundmanager;
 
-    [SerializeField] abstract AudioClip _clip;
+    [SerializeField]  AudioClip _clip;
 
-    protected abstract void TurnOn() { }
+    protected abstract void TurnOn();
 
-    protected abstract void TurnOff() { }
+    protected abstract void TurnOff();
 
-    protected abstract void Toggle() { }
+    protected abstract void Toggle();
 }
