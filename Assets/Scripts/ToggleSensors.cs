4051b042b8c4e987f40cb3fe0d1cf383f3f2399a
thederpygrandma <andreas.liebach@gmail.com>
2022-04-06 11:50:03 +0200

    Update toggle sensor

--- Assets/Scripts/ToggleSensors.cs
+++ Assets/Scripts/ToggleSensors.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class ToggleSensors : MonoBehaviour
 {
-    void Start()
+
+    /// If button is pressed, it will check wether or not sensor is on, and behave accordingly.
+    public void whenButtonClicked()
     {
-        InputSystem.EnableDevice(Gyroscope.current);
+        if (InputSystem.DisableDevice(Gyroscope.current); == true)
+            InputSystem.EnableDevice(Gyroscope.current);
+        else 
+            InputSystem.DisableDevice(Gyroscope.current);
         if (Gyroscope.current.enabled)
         Debug.Log("Gyroscope is enabled");
+
+        if (Gyroscope.current.disabled)
+        Debug.Log("Gyroscope is disabled");
     }
 }
