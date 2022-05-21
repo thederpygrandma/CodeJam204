d820570439b6865a668eab2cbcc422e523360413
LucJakobsen <lucjakobsen@gmail.com>
2022-05-21 13:26:26 +0200

    :squirrel:

---
+++ Assets/Scenes/Lucas ez/Scripts/CoordinatesText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CoordinatesText : MonoBehaviour
+{
+    public TextMeshProUGUI coordinates;
+
+    private void Update()
+    {
+        coordinates.text = "Latitude:" + GPSManager.Instance.latitude.ToString() + "   Longitude:" + GPSManager.Instance.longitude.ToString();
+    }
+}
---
+++ Assets/Scenes/Lucas ez/Scripts/GPSManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPSManager : SingletonPattern<GPSManager>
+{
+    public float latitude;
+    public float longitude;
+    private int maxWait = 20;
+
+    private IEnumerator Start()
+    {
+        if (!Input.location.isEnabledByUser)
+            yield break;
+
+        Input.location.Start();
+
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        if (maxWait <= 0)
+        {
+            print("Timed out");
+            yield break;
+        }
+
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            print("Unable to determine device location");
+            yield break;
+        }
+
+        latitude = Input.location.lastData.latitude;
+        longitude = Input.location.lastData.longitude;
+    }
+
+    
+}
